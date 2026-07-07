7cb74c63494898d740bd1d21caf860f5235ca134
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-07 09:56:56 -0300

    feat(navBar): adiciona links e icones com estilização condicional com o uso da biblioteca classNames

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,10 +1,39 @@
+import classNames from "classnames";
 import styles from "./nav.module.scss";
-import { reactComponent as Logo } from "@/assets/logo.svg";
+import Logo from "@/assets/logo.svg?react";
+import { RiShoppingCart2Line, RiShoppingCartFill } from "react-icons/ri";
+
+const iconeProps = {
+  color: "white",
+  size: 24,
+};
 
 export const NavBar = () => {
   return (
-    <nav classname={styles.nav}>
-      <Logo classname={styles.logo} />
+    <nav className={styles.nav}>
+      <Logo className={styles.logo} />
+      <div className={styles.links}>
+        <div>
+          <a
+            href="/"
+            className={classNames(styles.link, {
+              [styles.selected]: window.location.pathname === "/",
+            })}
+          >
+            Página Inicial
+          </a>
+        </div>
+      </div>
+      <div className={styles.busca}>
+        <a href="/carrinho">
+          {window.location.pathname === "/carrinho" ? (
+            <RiShoppingCartFill {...iconeProps} />
+          ) : (
+            <RiShoppingCart2Line {...iconeProps} />
+          )}{" "}
+        </a>
+      </div>
+      <div className={styles.icones}></div>
     </nav>
   );
 };
