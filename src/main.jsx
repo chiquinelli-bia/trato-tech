74c9ca5f9348d25cc297ea2e20753d5332a62c25
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-07 09:44:36 -0300

    feat(ambienteDeDesenvolvimento): mrigra para o vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { NavBar } from "./components/navBar";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div>Hello world</div>
+    <NavBar />
   </React.StrictMode>,
 );
