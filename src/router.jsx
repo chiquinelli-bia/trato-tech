1039da3b9c50ff7d1ed881ba3c419cfa288d1ef3
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-08 08:39:21 -0300

    feat(Home): cria e importa componente Home

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer";
 import PaginaPadrao from "./components/paginaPadrao";
+import Home from "./pages/home";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
-          <Route index element={<div></div>} />
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
