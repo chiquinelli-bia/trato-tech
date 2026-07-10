7e9ccb87eaf59f427a7b028a5a2bec482156ab19
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-10 09:28:48 -0300

    feat(redux): cria reducer para itens e faz upload dos arquivos do array

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriasReducer from "./reducers/categorias";
+import itensReducer from "./reducers/itens";
 
 const store = configureStore({
-  reducer: { categorias: categoriasReducer },
+  reducer: { categorias: categoriasReducer, itens: itensReducer },
 });
 
 export default store;
