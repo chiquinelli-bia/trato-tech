5ea2aea6f8ef80490640ef3b3b299554c8bef3ab
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-10 09:36:10 -0300

    feat(Categoria): cria e importa página de categoria com rota dinamica

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer";
 import PaginaPadrao from "./components/paginaPadrao";
 import Home from "./pages/home";
+import Categoria from "./pages/categorias";
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Home />} />
+          <Route path="/categoria/:nomeCategoria" element={<Categoria />} />
         </Route>
       </Routes>
     </BrowserRouter>
