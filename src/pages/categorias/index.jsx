1dfd43e75167d0905470a43f54a264c2f0d4266d
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-15 09:12:59 -0300

    feat(item): cria e importa componente de item

--- src/pages/categorias/index.jsx
+++ src/pages/categorias/index.jsx
@@ -1,13 +1,17 @@
 import { useParams } from "react-router-dom";
-import Header from "../../components/header";
+import Header from "@/components/header";
 import styles from "./categorias.module.scss";
 import { useSelector } from "react-redux";
+import Item from "@/components/item";
 
 export default function Categoria() {
   const { nomeCategoria } = useParams();
-  const categoria = useSelector((state) =>
-    state.categorias.find((categoria) => categoria.id === nomeCategoria),
-  );
+  const { categoria, itens } = useSelector((state) => ({
+    categoria: state.categorias.find(
+      (categoria) => categoria.id === nomeCategoria,
+    ),
+    itens: state.itens.filter((item) => item.categoria === nomeCategoria),
+  }));
   return (
     <div>
       <Header
@@ -15,6 +19,11 @@ export default function Categoria() {
         descricao={categoria.descricao}
         imagem={categoria.header}
       />
+      <div className={styles.itens}>
+        {itens?.map((item) => (
+          <Item key={item.id} {...item} />
+        ))}
+      </div>
     </div>
   );
 }
