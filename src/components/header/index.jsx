1ed40768a81d48565da3cd3ea79d924e95948b23
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-16 09:29:22 -0300

    fix(header): evita quebra de layout na ausência da imagem

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,21 @@
 import styles from "./header.module.scss";
+import TituloComImagem from "./tituloComImagem";
+import TituloSemImagem from "./tituloSemImagem";
 
 const Header = ({ className = "", titulo, descricao, imagem }) => {
   return (
     <header className={`${styles.header} ${className}`}>
-      <div className={styles["header-texto"]}>
-        <h1>{titulo}</h1>
-        <h2>{descricao}</h2>
-      </div>
-      <div className={styles["header-imagem"]}>
-        <img src={imagem} alt={titulo} />
-      </div>
+      {titulo && !imagem && (
+        <TituloSemImagem titulo={titulo} descricao={descricao} />
+      )}
+      {titulo && imagem && (
+        <TituloComImagem
+          titulo={titulo}
+          descricao={descricao}
+          imagem={imagem}
+          className={className}
+        />
+      )}
     </header>
   );
 };
