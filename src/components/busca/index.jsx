6f04f8d91e98f23908ce2c585b354458e7521104
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-17 09:49:19 -0300

    feat(Busca): adapta componente busca para utilização do reducer

--- src/components/busca/index.jsx
+++ src/components/busca/index.jsx
@@ -1,12 +1,24 @@
 import styles from "./busca.module.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { mudarBusca, resetarBusca } from "@/store/reducers/busca";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export const Busca = () => {
+  const busca = useSelector((state) => state.busca);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  useEffect(() => {
+    dispatch(resetarBusca());
+  }, [location.pathname, dispatch]);
   return (
     <div className={styles.busca}>
       <input
         type="search"
         placeholder="Oque você procura?"
         className={styles.input}
+        value={busca}
+        onChange={(Event) => dispatch(mudarBusca(Event.target.value))}
       />
     </div>
   );
