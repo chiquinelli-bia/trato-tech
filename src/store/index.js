f250317c3a9c5554f1c76756649fbdc9a9bb3cb9
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-17 09:48:32 -0300

    feat(redux): cria reducer busca

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import categoriasReducer from "./reducers/categorias";
 import itensReducer from "./reducers/itens";
 import carrinhoReducer from "./reducers/carrinho";
+import buscaReducer from "./reducers/busca";
 
 const store = configureStore({
   reducer: {
     categorias: categoriasReducer,
     itens: itensReducer,
     carrinho: carrinhoReducer,
+    busca: buscaReducer,
   },
 });
 
