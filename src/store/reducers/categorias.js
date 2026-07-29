d0057b77528b90b50d3d50ab4199d45bd649b056
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-29 10:34:04 -0300

    refactor(categoriasReducer): remove código não utilizado e limpa o estado antes da atualização

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,22 +1,11 @@
-import automotivoThumb from "@/assets/categorias/thumbnail/automotivo.png";
-import eletronicosThumb from "@/assets/categorias/thumbnail/eletronicos.png";
-import escritorioThumb from "@/assets/categorias/thumbnail/escritorio.png";
-import jogosThumb from "@/assets/categorias/thumbnail/jogos.png";
-import somThumb from "@/assets/categorias/thumbnail/som.png";
-import automotivoHeader from "@/assets/categorias/header/automotivo.png";
-import eletronicosHeader from "@/assets/categorias/header/eletronicos.png";
-import escritorioHeader from "@/assets/categorias/header/escritorio.png";
-import jogosHeader from "@/assets/categorias/header/jogos.png";
-import somHeader from "@/assets/categorias/header/som.png";
 import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = [];
 
 const categoriasSlice = createSlice({
   name: "categorias",
-  initialState,
+  initialState: [],
   reducers: {
     adicionarCategorias: (state, { payload }) => {
+      state.length = 0;
       state.push(...payload);
     },
   },
