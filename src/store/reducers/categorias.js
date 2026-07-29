3eb0715479666ff568420ac6be88878cbb82ca78
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-07-29 10:11:02 -0300

    feat(common): cria instancia do axios e exclui array de categorias e itens para buscar da instancia

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -10,44 +10,7 @@ import jogosHeader from "@/assets/categorias/header/jogos.png";
 import somHeader from "@/assets/categorias/header/som.png";
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  {
-    nome: "Eletrônicos",
-    thumbnail: eletronicosThumb,
-    header: eletronicosHeader,
-    id: "eletronicos",
-    descricao: "Os melhores e mais atuais dispositivos eletrônicos estão aqui!",
-  },
-  {
-    nome: "Automotivo",
-    thumbnail: automotivoThumb,
-    header: automotivoHeader,
-    id: "automotivos",
-    descricao:
-      "Encontre aqui equipamentos para deixar seu carro com a sua cara!",
-  },
-  {
-    nome: "Jogos",
-    thumbnail: jogosThumb,
-    header: jogosHeader,
-    id: "jogos",
-    descricao: "Adquira os consoles e jogos mais atuais do mercado !",
-  },
-  {
-    nome: "Escritório",
-    thumbnail: escritorioThumb,
-    header: escritorioHeader,
-    id: "escritorio",
-    descricao: "Tudo para o que escritório ficar incrível!",
-  },
-  {
-    nome: "Som e imagem",
-    thumbnail: somThumb,
-    header: somHeader,
-    id: "som",
-    descricao: "Curta suas músicas e seus filmes com a melhor qualidade!",
-  },
-];
+const initialState = [];
 
 const categoriasSlice = createSlice({
   name: "categorias",
