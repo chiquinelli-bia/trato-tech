abec296f971616292ce861fb80f4950fadef16dc
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-04 07:59:56 -0300

    feat(CategoriaService): adiciona carregamento assíncrono de categorias
    - cria thunk buscarCategorias com createAsyncThunk
    - integra categoriaService.buscar ao Redux
    - atualiza o estado ao concluir a busca usando extraReducers

---
+++ src/services/categoriaService.js
@@ -0,0 +1,12 @@
+import instance from "../common/config/api";
+
+const categoriasService = {
+  buscar: async () => {
+    const resposta = await instance.get("/categorias");
+
+    return resposta.data;
+    console.log(data);
+  },
+};
+
+export default categoriasService;
--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import categoriasService from "@/services/categoriaService.js";
+
+export const buscarCategorias = createAsyncThunk(
+  "categoria/buscar",
+  categoriasService.buscar,
+);
 
 const categoriasSlice = createSlice({
   name: "categorias",
@@ -8,6 +14,12 @@ const categoriasSlice = createSlice({
       state.length = 0;
       state.push(...payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(buscarCategorias.fulfilled, (state, { payload }) => {
+      state.length = 0;
+      state.push(...payload);
+    });
   },
 });
 
