fb455f33707fe6aba506ea9104909d9ee3a2049b
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-05 09:58:12 -0300

    feat(main): adiciona container Toast

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import "./index.css";
 import Router from "./router";
 import { Provider } from "react-redux";
 import store from "./store/index.js";
+import { createStandaloneToast } from "@chakra-ui/toast";
+
+const { ToastContainer } = createStandaloneToast();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <Router />
     </Provider>
+    <ToastContainer />
   </React.StrictMode>,
 );
