628723fde345b85edc4b6f7063b628407401da36
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-05 10:02:30 -0300

    feat(categoriasReducer): implementa toast aos tratamento dos estados de pending, fulfilled e rejected

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,5 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import categoriasService from "@/services/categoriaService.js";
+import { createStandaloneToast } from "@chakra-ui/toast";
+
+const { toast } = createStandaloneToast();
 
 export const buscarCategorias = createAsyncThunk(
   "categoria/buscar",
@@ -16,10 +19,36 @@ const categoriasSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(buscarCategorias.fulfilled, (state, { payload }) => {
-      state.length = 0;
-      state.push(...payload);
-    });
+    builder
+      .addCase(buscarCategorias.fulfilled, (state, { payload }) => {
+        toast({
+          title: "Sucesso!",
+          description: "Categorias carregadas com sucesso!",
+          status: "success",
+          duration: 2000,
+          isClosable: true,
+        });
+        state.length = 0;
+        return payload;
+      })
+      .addCase(buscarCategorias.pending, (state, { payload }) => {
+        toast({
+          title: "Carregando",
+          description: "Carregando categorias",
+          status: "loading",
+          duration: 2000,
+          isClosable: true,
+        });
+      })
+      .addCase(buscarCategorias.rejected, (state, { payload }) => {
+        toast({
+          title: "Erro",
+          description: "Erro na busca de categorias",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+      });
   },
 });
 
