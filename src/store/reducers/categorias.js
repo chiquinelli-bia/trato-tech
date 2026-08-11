568f07544329b3141cfa9ee348afe7f558b1f477
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-08-11 08:30:38 -0300

    feat(categoriasListener): adiciona toasts e remove lógica do reducer - Adiciona toasts ao listener de categorias - Remove a lógica de toast do reducerCategorias - Desinscreve o listener para evitar renderizações desnecessárias

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,8 +1,5 @@
 import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import categoriasService from "@/services/categoriaService.js";
-import { createStandaloneToast } from "@chakra-ui/toast";
-
-const { toast } = createStandaloneToast();
 
 export const buscarCategorias = createAsyncThunk(
   "categoria/buscar",
@@ -22,38 +19,6 @@ const categoriasSlice = createSlice({
     adicionarTodasAsCategorias: (state, { payload }) => {
       return payload;
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(buscarCategorias.fulfilled, (state, { payload }) => {
-        toast({
-          title: "Sucesso!",
-          description: "Categorias carregadas com sucesso!",
-          status: "success",
-          duration: 2000,
-          isClosable: true,
-        });
-        state.length = 0;
-        return payload;
-      })
-      .addCase(buscarCategorias.pending, (state, { payload }) => {
-        toast({
-          title: "Carregando",
-          description: "Carregando categorias",
-          status: "loading",
-          duration: 2000,
-          isClosable: true,
-        });
-      })
-      .addCase(buscarCategorias.rejected, (state, { payload }) => {
-        toast({
-          title: "Erro",
-          description: "Erro na busca de categorias",
-          status: "error",
-          duration: 2000,
-          isClosable: true,
-        });
-      });
   },
 });
 
